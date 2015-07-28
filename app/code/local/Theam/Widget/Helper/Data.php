e9fe9133fb1310fb0d484ed023f71cc86c8f8e9f
Mitch Vanderlinden <m.vanderlinden@geschenkidee.de>
2015-07-28 13:48:28 +0200

    Added module to handle Widgets

---
+++ app/code/local/Theam/Widget/Helper/Data.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * File: Data.php
+ *
+ * User: Mitch Vanderlinden
+ * email: mitchvdl@gmail.com
+ * Date: 28.07.15 09:53
+ * Package: Theam_Widget
+ */
+
+class Theam_Widget_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    
+}
+ 
