231e21abd5592d4b9ba11dc68abc1ebbf87248ed
Mitch Vanderlinden <m.vanderlinden@geschenkidee.de>
2015-08-03 16:05:15 +0200

    Seperate navigation module to make a customizable interface

---
+++ app/code/local/Theam/Navigation/Block/Html/Topmenu.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * File: Topmenu.php
+ *
+ * User: Mitch Vanderlinden
+ * email: mitchvdl@gmail.com
+ * Date: 03.08.15 15:55
+ * Package: Theam_Navigation
+ */
+
+class Theam_Navigation_Block_Html_Topmenu extends Mage_Page_Block_Html_Topmenu
+{
+
+}
+ 
---
+++ app/code/local/Theam/Navigation/Block/Html/Topmenu/Renderer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * File: Renderer.php
+ *
+ * User: Mitch Vanderlinden
+ * email: mitchvdl@gmail.com
+ * Date: 03.08.15 15:56
+ * Package: Theam_Navigation
+ */
+
+class Theam_Navigation_Block_Html_Topmenu_Renderer extends Mage_Page_Block_Html_Topmenu_Renderer
+{
+
+}
+ 
