37f8ffaa78ce88bbd11ce0170d2ddfb0459d8ade
Mitch Vanderlinden <m.vanderlinden@geschenkidee.de>
2015-08-03 16:43:24 +0200

    Base of custom MegaMenu module based on Bootstrap

---
+++ app/code/local/Theam/Navigation/Model/Resource/Setup.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * File: Setup.php
+ *
+ * User: Mitch Vanderlinden
+ * email: mitchvdl@gmail.com
+ * Date: 28.07.15 09:53
+ * Package: Theam_Navigation
+ */
+
+/**
+ * Class Theam_Navigation_Model_Resource_Setup
+ */
+class Theam_Navigation_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
+{
+
+}
