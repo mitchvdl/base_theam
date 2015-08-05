49fae8638247080326194befcb5110dea843e6f4
Mitch Vanderlinden <m.vanderlinden@geschenkidee.de>
2015-08-05 17:59:38 +0200

    Review section can be enabled/disabled in back-end

---
+++ app/code/local/Theam/Review/Helper/Data.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * File: Data.php
+ *
+ * User: Mitch Vanderlinden
+ * email: mitchvdl@gmail.com
+ * Date: 28.07.15 09:53
+ * Package: Theam_Review
+ */
+
+class Theam_Review_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    
+}
+ 
