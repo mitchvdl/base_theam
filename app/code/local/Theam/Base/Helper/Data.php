e71fa43c43f7846cefe13693ab427cc7c06edf0b
Mitch Vanderlinden <m.vanderlinden@geschenkidee.de>
2015-08-07 08:36:01 +0200

    Google map rendering block, re-usable accross all pages when params are set correctly

--- app/code/local/Theam/Base/Helper/Data.php
+++ app/code/local/Theam/Base/Helper/Data.php
@@ -10,6 +10,22 @@
 
 class Theam_Base_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    
+    const XPATH_STORE_INFO_MAP_ENABLED = 'general/store_information/display_map';
+    const XPATH_STORE_INFO_LONGITUDE = 'general/store_information/longitude';
+    const XPATH_STORE_INFO_LATITUDE = 'general/store_information/latitude';
+
+    public function isMapDisplayEnabled()
+    {
+        return Mage::getStoreConfigFlag(self::XPATH_STORE_INFO_MAP_ENABLED);
+    }
+
+    public function getLongitude()
+    {
+        return Mage::getStoreConfig(self::XPATH_STORE_INFO_LONGITUDE);
+    }
+
+    public function getLatitude()
+    {
+        return Mage::getStoreConfig(self::XPATH_STORE_INFO_LATITUDE);
+    }
 }
- 
